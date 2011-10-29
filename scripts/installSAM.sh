e806fa3d0859fc40fcd51fae0b926a0d73c69ccb
Philipp Haußleiter <philipp@haussleiter.de>
2011-10-29 17:16:51 +0200

    updating setup scripts

--- scripts/installSAM.sh
+++ scripts/installSAM.sh
@@ -2,7 +2,7 @@
 ROOT="https://github.com/phaus/sam.git"
 sudo apt-get -y install git-core
 GIT=`which git`
-PLAY=`which play`
+PLAY="/usr/local/play/play"
 cd /usr/local
 echo "Checkout SAM"
 sudo $GIT clone $ROOT
