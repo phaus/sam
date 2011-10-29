d6fc99ecbd2ca0a2874b2a7c7bb91df52b0ba1ab
Philipp Haußleiter <philipp@haussleiter.de>
2011-10-29 17:33:51 +0200

    updating install script

--- scripts/installSAM.sh
+++ scripts/installSAM.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
+echo "========================================"
+echo "  preparing system	              "
+echo "========================================"
+sudo apt-get -y install git-core ssh-keygen
 ROOT="https://github.com/phaus/sam.git"
-sudo apt-get -y install git-core
 GIT=`which git`
 PLAY="/usr/local/play/play"
 cd /usr/local
-echo "Checkout SAM"
+echo "========================================"
+echo "  Checkout latest SAM		      "
+echo "========================================"
 sudo $GIT clone $ROOT
-echo "Starting SAM"
-$PLAY start /usr/local/sam
+echo "========================================"
+echo "  SSH Setup - user needs access as root "
+echo "  USE empty Passphrase for this key     "
+echo "========================================"
+ssh-keygen -q -t rsa -b 2048 -f ~/.ssh/id_rsa
+sudo mkdir -p /root/.ssh/
+sudo cat ~/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+sudo chmod 0600 /root/.ssh/authorized_keys
+echo "========================================"
+echo " Starting SAM			      "
+echo "========================================"
+sudo mkdir -p /var/log/sam
+sudo ln -s /var/log/sam logs
+sudo $PLAY start /usr/local/sam
 
