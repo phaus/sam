f45c4077c21fc40166fa115bf8330ffe55168d3e
Philipp Haußleiter <philipp@haussleiter.de>
2011-11-12 01:17:26 +0100

    Update scripts/apt/playSetup.sh

--- scripts/apt/playSetup.sh
+++ scripts/apt/playSetup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y install openjdk-6-jre-headless unzip
-cd /tmp
-wget http://download.playframework.org/releases/play-1.2.3.zip
-sudo unzip play-1.2.3.zip -d /usr/local/
+sudo apt-get -y install openjdk-6-jre-headless unzip python
+wget http://download.playframework.org/releases/play-1.2.2.zip -O ~/samt/packages/play-1.2.2.zip
+sudo unzip ~/samt/packages/play-1.2.2.zip -d /usr/local/
 cd /usr/local
-sudo ln -s play-1.2.3 play
+sudo ln -s play-1.2.2 play
 sudo chmod +x /usr/local/play/play
 sudo ln -s /usr/local/play/play /usr/bin/play
