c2220a0037d5dcb2150e7ebf4c13aa60be0dd783
Philipp Haußleiter <philipp@haussleiter.de>
2012-04-03 22:36:44 +0200

    fixing sam start error

--- scripts/apt/installSAM.sh
+++ scripts/apt/installSAM.sh
@@ -36,17 +36,19 @@ sudo $GIT clone $ROOT $SAM_INSTALL_DIR/sam
 echo "========================================"
 echo " Starting SAM                  "
 echo "========================================"
+
 sudo mkdir -p /var/log/sam
-sudo ln -s /var/log/sam logs
+sudo ln -s /var/log/sam $SAM_INSTALL_DIR/sam/logs
 
 echo "#!/bin/bash\n" > $SAM_INSTALL_DIR/sam/start-sam.sh
 echo "$PLAY start $SAM_INSTALL_DIR/sam --%prod -Xmx64M" >> $SAM_INSTALL_DIR/sam/start-sam.sh
+sudo chmod +x $SAM_INSTALL_DIR/sam/start-sam.sh
 
 echo "#!/bin/bash\n" > $SAM_INSTALL_DIR/sam/stop-sam.sh
 echo "$PLAY stop $SAM_INSTALL_DIR/sam" >> $SAM_INSTALL_DIR/sam/stop-sam.sh
-
-sudo chmod +x $SAM_INSTALL_DIR/sam/start-sam.sh
 sudo chmod +x $SAM_INSTALL_DIR/sam/stop-sam.sh
-sudo $SAM_INSTALL_DIR/sam/start-sam.sh
+
+
+sudo bash $SAM_INSTALL_DIR/sam/start-sam.sh
 
 showIps
